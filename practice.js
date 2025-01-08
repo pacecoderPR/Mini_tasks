7e49b104f3007bbbb994bf69280ef06bad07c7a7
Pritesh Rajpurohit <priteshrajpurohit85@gmail.com>
2025-01-08 09:49:31 +0530

    form validation

--- practice.js
+++ practice.js
@@ -1,12 +1,31 @@
+let formValidate= (event)=>{
+    console.log(event)
+    event.preventDefault();
+    const form =event.target;
+    let isValid=true;
+    for(let i=0;i<form.elements.length;i++){
+        const input=form.elements[i];
+        const value=input.value.trim();
 
-function handleData(id){
-    console.log(id);
-    let el=document.getElementById(id);
-    console.log(el);
-    if(el.style.display==="none"){
-        el.style.display="block";
+        if(input.hasAttribute("required")&& value===""){
+            isValid=false;
+            input.classList.add("invalid")
+        }
+        else{
+            input.classList.remove("invalid");
+        }
+    }
+
+    const message=document.getElementById("message");
+    if(isValid){
+        message.classList.remove("error");
+        message.textContent="Form submitted successfully";
     }
     else{
-        el.style.display="none";
+        message.classList.add("error");
+        message.innerText="Please fill out the required fields";
     }
-}
+};
+const form=document.getElementById("myForm");
+console.log(form);
+form.addEventListener("submit",formValidate)
